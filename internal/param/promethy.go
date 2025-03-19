2911c821cfab1e3b2e121884ba0f955dcf8eed30
storezhang <storezhang@gmail.com>
2025-03-19 09:30:13 +0800

    feat(参数): 默认增加namespace、pod、node、ip等环境变量

--- internal/param/promethy.go
+++ internal/param/promethy.go
@@ -1,6 +1,7 @@
 package param
 
 import (
+	"github.com/goexl/env"
 	"github.com/goexl/log"
 	"github.com/goexl/valuer"
 	"github.com/prometheus/client_golang/prometheus"
@@ -18,6 +19,11 @@ func NewParams() *Promethy {
 		Logger:   log.New().Apply(),
 		Registry: prometheus.NewRegistry(),
 		Parser:   valuer.New().Build(),
-		Labels:   make(map[string]string),
+		Labels: map[string]string{
+			"namespace": env.Get("NAMESPACE"), // 命名空间
+			"pod":       env.Get("POD"),       // 容器
+			"node":      env.Get("NODE"),      // 节点
+			"ip":        env.Get("IP"),        // 地址
+		},
 	}
 }
